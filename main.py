c1014ec38f607643114fddbee4e6f7c7deeadd23
Mikhail Volf <misha.volf93@gmail.com>
2022-11-05 00:51:04 +0300

    Добавлены комментарии и исправлена ошибка справнения

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Класс студент
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -7,6 +8,7 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    # Выставление оценок за лекции
     def rate_hw(self, mentor, course, grade):
         if isinstance(mentor, Lecturer) and course in mentor.courses_attached\
            and course in self.courses_in_progress:
@@ -17,6 +19,7 @@ class Student:
         else:
             return 'Ошибка'
 
+    # Вывод информации о студенте
     def __str__(self):
         view_name = f'Имя: {self.name}'
         view_surname = f'Фамилия: {self.surname}'
@@ -25,31 +28,25 @@ class Student:
         view__finish_courses = f'Завершенные курсы: {", ".join(self.finish_courses) if self.finish_courses else "отсутствуют"}'
         return f'{view_name}\n{view_surname}\n{view_rating}\n{view_cources_in_progress}\n{view__finish_courses}'
 
+    # Операторы сравнения
     def __eq__(self, student):
-        return self.__comparison(student, 'eq')
+        return average_rating(self) == average_rating(student)
 
     def __lt__(self, student):
-        return self.__comparison(student, 'lt')
+        return average_rating(self) < average_rating(student)
 
     def __gt__(self, student):
-        return self.__comparison(student, 'gt')
-
-    def __comparison(self, student, command='eq'):
-        match command:
-            case 'eq':
-                return average_rating(self) == average_rating(student)
-            case 'lt':
-                return average_rating(self) < average_rating(student)
-            case 'gt':
-                return average_rating(self) > average_rating(student)
+        return average_rating(self) > average_rating(student)
 
 
+# Класс Ментор
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+    # Метод выставления оценок студентам
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached\
            and course in student.courses_in_progress:
@@ -61,36 +58,31 @@ class Mentor:
             return 'Ошибка'
 
 
+# Класс лектор (наследован от лектора)
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
+    # Функция вывода информации о классе
     def __str__(self):
         self.view_name = 'Имя: ' + self.name
         self.view_surname = 'Фамилия: ' + self.surname
         view_rate = f'Средняя оценка за лекции: {average_rating(self)}'
         return f'{self.view_name}\n{self.view_surname}\n{view_rate}'
 
+    # Операторы сравнения
     def __eq__(self, lecturer):
-        return self.__comparison(lecturer, 'eq')
+        return average_rating(self) == average_rating(lecturer)
 
     def __lt__(self, lecturer):
-        return self.__comparison(lecturer, 'lt')
+        return average_rating(self) < average_rating(lecturer)
 
     def __gt__(self, lecturer):
-        return self.__comparison(lecturer, 'gt')
-
-    def __comparison(self, lecturer, command='eq'):
-        match command:
-            case 'eq':
-                return average_rating(self) == average_rating(lecturer)
-            case 'lt':
-                return average_rating(self) < average_rating(lecturer)
-            case 'gt':
-                return average_rating(self) > average_rating(lecturer)
+        return average_rating(self) > average_rating(lecturer)
 
 
+# Класс лектор (наследован от лектора)
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -99,6 +91,8 @@ class Reviewer(Mentor):
         return f'Имя: {self.name}\nФамилия: {self.surname}'
 
 
+# Функция сравнения оценок по курсам у перечня классов,
+# универсальная для всех классов
 def comparison_course(all_people, course):
     all_rating, number_people = 0, 0
     for people in all_people:
@@ -107,28 +101,33 @@ def comparison_course(all_people, course):
             number_people += 1
     return f'По курсу {course} средняя оценка: {all_rating/number_people}'
 
+
+# Функция сравнения оценок между учениками или преподавателями,
+# универсальная для всех классов
 def average_rating(Class, course=''):
-    course = Class.grades.keys() if course=='' else course
+    course = list(Class.grades.keys()) if course == '' else course
     sum_rating, values = 0, 0
     for class_courses in Class.grades.keys():
         if class_courses in course:
-            sum_rating += sum(Class.grades[course])
-            values += len(Class.grades[course])
+            sum_rating += sum(Class.grades[class_courses])
+            values += len(Class.grades[class_courses])
     if Class.grades:
         return round(sum_rating/values, 1)
     else:
         return 0
 
+
+# Заполнение Базы примеров
 def main():
     student_Evgeniya = Student('Evgeniya', 'Daranovski', 'woman')
     student_Evgeniya.courses_in_progress += ['Python', 'Git']
-
     student_Mikhail = Student('Mikhail', 'Volf', 'man')
     student_Mikhail.courses_in_progress += ['Python', 'Git']
+    student_Vasilii = Student('Vasilii', 'Pupkin', 'man')
+    student_Vasilii.courses_in_progress += ['Git']
 
     lector_Grigori = Lecturer('Grigoriy', 'Sandrikov')
     lector_Grigori.courses_attached += ['Python']
-
     lector_Natali = Lecturer('Natali', 'Bayer')
     lector_Natali.courses_attached += ['Git']
 
@@ -139,21 +138,27 @@ def main():
     student_Evgeniya.rate_hw(lector_Natali, 'Git', 9)
     student_Mikhail.rate_hw(lector_Grigori, 'Python', 10)
     student_Mikhail.rate_hw(lector_Natali, 'Git', 7)
+    student_Vasilii.rate_hw(lector_Natali, 'Git', 10)
     lector_Grigori.rate_hw(student_Evgeniya, 'Python', 9)
     lector_Grigori.rate_hw(student_Evgeniya, 'Python', 8)
     lector_Grigori.rate_hw(student_Mikhail, 'Python', 10)
     lector_Grigori.rate_hw(student_Mikhail, 'Python', 10)
     lector_Natali.rate_hw(student_Mikhail, 'Git', 8)
     lector_Natali.rate_hw(student_Evgeniya, 'Git', 9)
+    lector_Natali.rate_hw(student_Vasilii, 'Git', 10)
     reviewer_Serj.rate_hw(student_Mikhail, 'Git', 4)
     reviewer_Serj.rate_hw(student_Evgeniya, 'Git', 10)
 
     all_student = [student_Mikhail, student_Evgeniya]
     all_mentors = [lector_Grigori, lector_Natali]
-    # print(student_Mikhail < student_Evgeniya)
+
+    print(student_Mikhail < student_Evgeniya)
     print(average_rating(student_Evgeniya, 'Python'))
     print(average_rating(student_Mikhail, 'Python'))
     print(comparison_course(all_student, 'Git') + ' за ДЗ')
     print(comparison_course(all_mentors, 'Git') + ' за лекции')
+    print(lector_Grigori > lector_Natali)
+    print(student_Evgeniya < student_Mikhail)
+
     
 main()
